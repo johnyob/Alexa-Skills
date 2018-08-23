616c7a8fef71bc3d12c45628965a4b974873bb28
johnyob <johnyob132@gmail.com>
2018-08-23 13:42:38 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -16,7 +16,7 @@ packages = [
 
 setuptools.setup(
     name="alexa-skills",
-    version="0.0.1",
+    version="0.0.2",
     author="Alistair O'Brien",
     author_email="alistair.o'brien@ellesmere.com",
     description="A Python Alexa Skills Package.",
