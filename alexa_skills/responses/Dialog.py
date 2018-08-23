10e5cc312ce97636243bf135ead5884b48d8a546
johnyob <johnyob132@gmail.com>
2018-08-23 13:42:19 +0100

    Update Dialog.py. Fixed updateIntent is Struct object bug.

--- alexa_skills/responses/Dialog.py
+++ alexa_skills/responses/Dialog.py
@@ -1,8 +1,14 @@
+import json
+
+
 class Dialog:
 
     def __init__(self, updated_intent):
         self._type = "Dialog.Delegate"
-        self._updated_intent = updated_intent
+        self._updated_intent = json.loads(json.dumps(
+            updated_intent,
+            default=lambda x: getattr(x, '__dict__', str(x))
+        ))
 
     def build(self):
         return {
